d3bbf0dd80b22131ce3b27ecd14ee49a5c7c5f89
FacundoVillarroel <facu.villarroel96@gmail.com>
2022-07-18 17:20:32 +0200

    Delete Customer

--- src/components/custormer/Customer.js
+++ src/components/custormer/Customer.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {BsFillTrashFill} from "react-icons/bs";
+import { doc, getDoc, collection, getDocs, writeBatch } from 'firebase/firestore';
+import {db} from "../../firebase/config";
 
 import "./customer.css";
+import { UserContext } from '../../context/UserContext';
 
 const Customer = ({customer}) => {
+
+  const { currentUser, usersList, setUsersList } = useContext(UserContext)
   
   const {name, address, nextCleaning, hours, frequency} = customer
 
+  const removeItem = async (name, usersList, currentUser) => {
+    const userId = (usersList.find(user => user.name === currentUser)).id
+    
+    const batch = writeBatch(db);
+    const docRef = doc(db , "usersList", userId)
+    const userFirebase = (await getDoc(docRef))
+    const customers = userFirebase.data().customers
+    const updatedCustomers = customers.filter(customer => customer.name !== name);
+    batch.update(userFirebase.ref, {
+      customers: updatedCustomers
+    })
+    await batch.commit()
+    const usersListRef = collection( db, "usersList" );
+    
+    getDocs( usersListRef )
+      .then( resp => {
+        const usersListFirebase = resp.docs.map((doc) => ({id:doc.id, ...doc.data()}))
+        setUsersList(usersListFirebase)
+      })
+  }
+
   return (
     <div className='customerContainer'>
-      <h2 className='text-center mb-5 mt-3'>{name}</h2>
+      <div className='customerName'>
+        <h2 className='text-center mb-5 mt-3'>{name} :</h2>
+        <button onClick={() => removeItem(name, usersList, currentUser)} className="btnDeleteUser"> <BsFillTrashFill/></button>
+      </div>
       <div className='customerData'>
         <div>
           <span className='bold'>Address:</span> <p className='info'>{address}</p></div>
